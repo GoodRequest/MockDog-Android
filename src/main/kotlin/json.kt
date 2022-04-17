25d7b961e6e9ab4f15b947b5e5f923c433cc9180
jozefknazko <jozef.knazko@goodrequest.com>
2022-04-17 15:18:06 +0200

    2 spaces reformat

--- src/main/kotlin/json.kt
+++ src/main/kotlin/json.kt
@@ -63,107 +63,107 @@ val textSize = 12.sp
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun JsonTree(root: JsonElement, key: String?, path: String, collapsed: SnapshotStateList<String>, onKeySelected: (String) -> Unit) {
-    val offset = 15.dp
-    val fullPath = if(key != null) "$path/$key" else path
+  val offset = 15.dp
+  val fullPath = if(key != null) "$path/$key" else path
 
-    when {
-        root.isJsonArray -> {
-            Column(modifier = Modifier.padding(horizontal = offset, vertical = 4.dp)) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    if(key != null)
-                        Text("$key: ", color = keyColor, fontSize = textSize)
+  when {
+    root.isJsonArray -> {
+      Column(modifier = Modifier.padding(horizontal = offset, vertical = 4.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+          if(key != null)
+            Text("$key: ", color = keyColor, fontSize = textSize)
 
-                    Text("[", color = bracesColor, fontSize = textSize)
+          Text("[", color = bracesColor, fontSize = textSize)
 
-                    if(root.asJsonArray.isEmpty.not())
-                        Icon(
-                            modifier = Modifier
-                                .size(24.dp)
-                                .rotate(if(fullPath in collapsed) 270f else 0f)
-                                .clickable {
-                                    if(fullPath in collapsed) collapsed.remove(fullPath)
-                                    else                      collapsed.add(fullPath)
-                                },
-                            imageVector         = Icons.Default.ArrowDropDown,
-                            contentDescription  = "Search",
-                            tint                = keyColor)
+          if(root.asJsonArray.isEmpty.not())
+            Icon(
+              modifier = Modifier
+                .size(24.dp)
+                .rotate(if(fullPath in collapsed) 270f else 0f)
+                .clickable {
+                  if(fullPath in collapsed) collapsed.remove(fullPath)
+                  else                      collapsed.add(fullPath)
+                },
+              imageVector         = Icons.Default.ArrowDropDown,
+              contentDescription  = "Search",
+              tint                = keyColor)
 
-                    if(fullPath in collapsed) {
-                        Text(if(root.asJsonArray.isEmpty) "]" else "${root} ]",
-                            color = bracesColor,
-                            fontSize = textSize,
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1)
-                    }
-                }
-                if(fullPath !in collapsed) {
-                    root.asJsonArray.forEachIndexed { index, item ->
-                        JsonTree(item, null, "$fullPath[$index]", collapsed, onKeySelected)
-                    }
-                    Text("]", color = bracesColor, fontSize = textSize)
-                }
-            }
+          if(fullPath in collapsed) {
+            Text(if(root.asJsonArray.isEmpty) "]" else "${root} ]",
+              color = bracesColor,
+              fontSize = textSize,
+              overflow = TextOverflow.Ellipsis,
+              maxLines = 1)
+          }
         }
-        root.isJsonObject -> {
-            Column(modifier = Modifier.padding(horizontal = offset, vertical = 4.dp)) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    if(key != null) Text(
-                        modifier = Modifier.clickable { onKeySelected("$fullPath") },
-                        text     = "$key: ",
-                        fontSize = textSize,
-                        color    = keyColor)
-
-                    Text("{", color = bracesColor, fontSize = textSize)
-
-                    if(root.asJsonObject.size() != 0)
-                        Icon(
-                            modifier = Modifier
-                                .size(24.dp)
-                                .rotate(if(fullPath in collapsed) 270f else 0f)
-                                .combinedClickable(
-                                    onClick     = {
-                                        if(fullPath in collapsed) collapsed.remove(fullPath)
-                                        else                      collapsed.add(fullPath)
-                                    },
-                                    onLongClick = {
-                                        val children = root.asJsonObject.entrySet().map { (key, _) -> "$fullPath/$key" }
-                                        collapsed.addAll(children)
-                                    }),
-                            imageVector         = Icons.Default.ArrowDropDown,
-                            contentDescription  = "Search",
-                            tint                = keyColor)
-
-                    if(fullPath in collapsed) {
-                        Text(
-                            text = if(root.asJsonObject.size() == 0) "}" else "$root }",
-                            fontSize = textSize,
-                            color = bracesColor,
-                            overflow = TextOverflow.Ellipsis,
-                        maxLines = 1)
-                    }
-                }
-                if(fullPath !in collapsed) {
-                    root.asJsonObject.entrySet().forEach { (key, value) ->
-                        JsonTree(value, key, fullPath, collapsed, onKeySelected)
-                    }
-                    Text("}", color = bracesColor, fontSize = textSize)
-                }
-            }
+        if(fullPath !in collapsed) {
+          root.asJsonArray.forEachIndexed { index, item ->
+            JsonTree(item, null, "$fullPath[$index]", collapsed, onKeySelected)
+          }
+          Text("]", color = bracesColor, fontSize = textSize)
         }
-        else -> {
-            Row(modifier = Modifier.padding(horizontal = offset)) {
-                Text(
-                    text     = "$key: ",
-                    fontSize = textSize,
-                    color    = keyColor)
-                Text(
-                    text         = "$root",
-                   // onValueChange = {},
-                    fontSize = textSize,
-                    color = PrimeBlack
-                )
-            }
-        }
+      }
     }
+    root.isJsonObject -> {
+      Column(modifier = Modifier.padding(horizontal = offset, vertical = 4.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+          if(key != null) Text(
+            modifier = Modifier.clickable { onKeySelected("$fullPath") },
+            text     = "$key: ",
+            fontSize = textSize,
+            color    = keyColor)
+
+          Text("{", color = bracesColor, fontSize = textSize)
+
+          if(root.asJsonObject.size() != 0)
+            Icon(
+              modifier = Modifier
+                .size(24.dp)
+                .rotate(if(fullPath in collapsed) 270f else 0f)
+                .combinedClickable(
+                  onClick     = {
+                    if(fullPath in collapsed) collapsed.remove(fullPath)
+                    else                      collapsed.add(fullPath)
+                  },
+                  onLongClick = {
+                    val children = root.asJsonObject.entrySet().map { (key, _) -> "$fullPath/$key" }
+                    collapsed.addAll(children)
+                  }),
+              imageVector         = Icons.Default.ArrowDropDown,
+              contentDescription  = "Search",
+              tint                = keyColor)
+
+          if(fullPath in collapsed) {
+            Text(
+              text = if(root.asJsonObject.size() == 0) "}" else "$root }",
+              fontSize = textSize,
+              color = bracesColor,
+              overflow = TextOverflow.Ellipsis,
+              maxLines = 1)
+          }
+        }
+        if(fullPath !in collapsed) {
+          root.asJsonObject.entrySet().forEach { (key, value) ->
+            JsonTree(value, key, fullPath, collapsed, onKeySelected)
+          }
+          Text("}", color = bracesColor, fontSize = textSize)
+        }
+      }
+    }
+    else -> {
+      Row(modifier = Modifier.padding(horizontal = offset)) {
+        Text(
+          text     = "$key: ",
+          fontSize = textSize,
+          color    = keyColor)
+        Text(
+          text         = "$root",
+          // onValueChange = {},
+          fontSize = textSize,
+          color = PrimeBlack
+        )
+      }
+    }
+  }
 }
 
