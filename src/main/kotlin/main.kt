4c426fce64c2c3d6523d3ae0636f11f135125557
Jozef Vivodik <jozef.vivodik@goodrequest.com>
2023-06-30 13:56:59 +0200

    implement dark theme switcher
    Zmena temy bude v settingoch a zaroven sa bude aj ukladat ale az po mergnuty branche kde to uz je nachystane aby sa nerobilo 2x to iste.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.input.key.*
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
@@ -14,6 +15,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.skiko.hostOs
 import ui.App
+import ui.isDarkTheme
 import ui.showSearch
 
 val windowState = WindowState(size = DpSize(1920.dp, 1080.dp))
@@ -34,7 +36,20 @@ fun main() {
           true
         } else false
       }
-    ) { App() }
+    ) {
+      MenuBar {
+        Menu("Settings") {
+          CheckboxItem(
+            text            = "Dark theme",
+            checked         = isDarkTheme.value,
+            onCheckedChange = { isDarkTheme.value = !isDarkTheme.value }
+          )
+
+        }
+      }
+
+      App()
+    }
 
     // Registracia globalneho hanglera na neodchytene pady app
     Thread.setDefaultUncaughtExceptionHandler(::saveUncaughtException)
